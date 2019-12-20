54b3ba13997ecb2696ad71f36ae4f931bd7a4a25
panmin2256 <panmin2256@ftsafe.com>
2019-12-20 11:26:13 +0800

    Fix the bugs of test.

--- test/JUB_SDK_test/JUB_SDK_test_btc.cpp
+++ test/JUB_SDK_test/JUB_SDK_test_btc.cpp
@@ -254,6 +254,7 @@ void transaction_test(JUB_UINT16 contextID, Json::Value root) {
 
         for (int i = 0; i < inputNumber; i++) {
             INPUT_BTC input;
+            input.type = JUB_ENUM_SCRIPT_BTC_TYPE::P2PKH;
             input.preHash = (char*)root["inputs"][i]["preHash"].asCString();
             input.preIndex = root["inputs"][i]["preIndex"].asInt();
             input.path.change = (JUB_ENUM_BOOL)root["inputs"][i]["bip32_path"]["change"].asBool();
@@ -266,13 +267,13 @@ void transaction_test(JUB_UINT16 contextID, Json::Value root) {
 
         for (int i = 0; i < outputNumber; i++) {
             OUTPUT_BTC output;
-            output.type = OUTPUT_ENUM_BTC_TYPE::STANDARD;
-            output.outputStandard.address = (char*)root["outputs"][i]["address"].asCString();
-            output.outputStandard.amount = root["outputs"][i]["amount"].asUInt64();
-            output.outputStandard.changeAddress = (JUB_ENUM_BOOL)root["outputs"][i]["change_address"].asBool();
-            if (output.outputStandard.changeAddress) {
-                output.outputStandard.path.change = (JUB_ENUM_BOOL)root["outputs"][i]["bip32_path"]["change"].asBool();
-                output.outputStandard.path.addressIndex = root["outputs"][i]["bip32_path"]["addressIndex"].asInt();
+            output.type = JUB_ENUM_SCRIPT_BTC_TYPE::P2PKH;
+            output.stdOutput.address = (char*)root["outputs"][i]["address"].asCString();
+            output.stdOutput.amount = root["outputs"][i]["amount"].asUInt64();
+            output.stdOutput.changeAddress = (JUB_ENUM_BOOL)root["outputs"][i]["change_address"].asBool();
+            if (output.stdOutput.changeAddress) {
+                output.stdOutput.path.change = (JUB_ENUM_BOOL)root["outputs"][i]["bip32_path"]["change"].asBool();
+                output.stdOutput.path.addressIndex = root["outputs"][i]["bip32_path"]["addressIndex"].asInt();
             }
             outputs.push_back(output);
         }
@@ -321,6 +322,7 @@ void transactionUSDT_test(JUB_UINT16 contextID, Json::Value root) {
 
         for (int i = 0; i < inputNumber; i++) {
             INPUT_BTC input;
+            input.type = JUB_ENUM_SCRIPT_BTC_TYPE::P2PKH;
             input.preHash = (char*)root["inputs"][i]["preHash"].asCString();
             input.preIndex = root["inputs"][i]["preIndex"].asInt();
             input.path.change = (JUB_ENUM_BOOL)root["inputs"][i]["bip32_path"]["change"].asBool();
@@ -333,13 +335,13 @@ void transactionUSDT_test(JUB_UINT16 contextID, Json::Value root) {
 
         for (int i = 0; i < outputNumber; i++) {
             OUTPUT_BTC output;
-            output.type = OUTPUT_ENUM_BTC_TYPE::STANDARD;
-            output.outputStandard.address = (char*)root["outputs"][i]["address"].asCString();
-            output.outputStandard.amount = root["outputs"][i]["amount"].asUInt64();
-            output.outputStandard.changeAddress = (JUB_ENUM_BOOL)root["outputs"][i]["change_address"].asBool();
-            if (output.outputStandard.changeAddress) {
-                output.outputStandard.path.change = (JUB_ENUM_BOOL)root["outputs"][i]["bip32_path"]["change"].asBool();
-                output.outputStandard.path.addressIndex = root["outputs"][i]["bip32_path"]["addressIndex"].asInt();
+            output.type = JUB_ENUM_SCRIPT_BTC_TYPE::P2PKH;
+            output.stdOutput.address = (char*)root["outputs"][i]["address"].asCString();
+            output.stdOutput.amount = root["outputs"][i]["amount"].asUInt64();
+            output.stdOutput.changeAddress = (JUB_ENUM_BOOL)root["outputs"][i]["change_address"].asBool();
+            if (output.stdOutput.changeAddress) {
+                output.stdOutput.path.change = (JUB_ENUM_BOOL)root["outputs"][i]["bip32_path"]["change"].asBool();
+                output.stdOutput.path.addressIndex = root["outputs"][i]["bip32_path"]["addressIndex"].asInt();
             }
             outputs.push_back(output);
         }
