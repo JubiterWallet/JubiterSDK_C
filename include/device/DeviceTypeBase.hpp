551d7795a120e14cea28a764ea017bca2bf85fbb
panmin2256 <panmin2256@ftsafe.com>
2020-01-14 11:17:40 +0800

    Handle warning.

--- include/device/DeviceTypeBase.hpp
+++ include/device/DeviceTypeBase.hpp
@@ -10,22 +10,25 @@
 namespace jub {
 namespace device {
 
-	class DeviceTypeBase {
+class DeviceTypeBase {
 
-	public:
-		/* functions */
-		virtual JUB_RV Connect() = 0;
-		virtual JUB_RV Disconnect() = 0;
+public:
+    virtual ~DeviceTypeBase() = 0;
 
-		virtual JUB_RV SendData(IN JUB_BYTE_CPTR sendData, IN JUB_ULONG ulSendLen,
-			OUT JUB_BYTE_PTR retData, INOUT JUB_ULONG_PTR pulRetDataLen,
-			IN JUB_ULONG ulMiliSecondTimeout = 1200000) = 0;
-	}; // class DeviceTypeBase end
+    /* functions */
+    virtual JUB_RV Connect() = 0;
+    virtual JUB_RV Disconnect() = 0;
+
+    virtual JUB_RV SendData(IN JUB_BYTE_CPTR sendData, IN JUB_ULONG ulSendLen,
+                            OUT JUB_BYTE_PTR retData, INOUT JUB_ULONG_PTR pulRetDataLen,
+                            IN JUB_ULONG ulMiliSecondTimeout = 1200000) = 0;
+}; // class DeviceTypeBase end
 
 #define FT3KHN_READWRITE_SIZE_ONCE_NEW 4096
 
-	using DeviceManager = Singleton<xManager<DeviceTypeBase>>;
-} // namespace jub device
+using DeviceManager = Singleton<xManager<DeviceTypeBase>>;
+
+} // namespace device end
 } // namespace jub end
 
 #endif // __DeviceTypeBase__
