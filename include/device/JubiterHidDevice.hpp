20889f4ed1fd0daa29a374ec784a45644b1a9ccd
panmin2256 <panmin2256@ftsafe.com>
2020-01-14 11:17:40 +0800

    Handle warning.

--- include/device/JubiterHidDevice.hpp
+++ include/device/JubiterHidDevice.hpp
@@ -11,51 +11,52 @@
 #include "device/DeviceTypeBase.hpp"
 
 namespace jub {
-	namespace device {
+namespace device {
 
 #define VID 0x096e
 #define PID 0x0891
 
-#define HID_PACKET_SIZE 65
-#define HID_PACKET_HEAD_FRIST 8
-#define HID_PACKET_HEAD_ROUND 6
+#define HID_PACKET_SIZE         65
+#define HID_PACKET_HEAD_FRIST   8
+#define HID_PACKET_HEAD_ROUND   6
 
-#define HID_PACKET_RHEAD_FRIST 7
-#define HID_PACKET_RHEAD_ROUND 5
+#define HID_PACKET_RHEAD_FRIST  7
+#define HID_PACKET_RHEAD_ROUND  5
 
 
-#define HID_PACKET_GNU_SIZE 64
-#define FIDO2_WAIT_FLAG     0xbb
+#define HID_PACKET_GNU_SIZE     64
+#define FIDO2_WAIT_FLAG         0xbb
 
-		class JubiterHidDevice : public DeviceTypeBase {
+class JubiterHidDevice
+    : public DeviceTypeBase {
 
-		public:
-			JubiterHidDevice(const std::string path);
-			~JubiterHidDevice();
+public:
+    JubiterHidDevice(const std::string path);
+    ~JubiterHidDevice();
 
-		public:
-			static  std::vector<std::string> EnumDevice();
-			virtual JUB_RV Connect();
-			virtual JUB_RV Disconnect();
+public:
+    static  std::vector<std::string> EnumDevice();
+    virtual JUB_RV Connect();
+    virtual JUB_RV Disconnect();
 
-			virtual JUB_RV SendData(IN JUB_BYTE_CPTR sendData, IN JUB_ULONG ulSendLen,
-				OUT JUB_BYTE_PTR retData, INOUT JUB_ULONG_PTR pulRetDataLen,
-				IN JUB_ULONG ulMiliSecondTimeout = 1200000);
-			std::string getPath() {return _path;};
+    virtual JUB_RV SendData(IN JUB_BYTE_CPTR sendData, IN JUB_ULONG ulSendLen,
+                            OUT JUB_BYTE_PTR retData, INOUT JUB_ULONG_PTR pulRetDataLen,
+                            IN JUB_ULONG ulMiliSecondTimeout = 1200000);
+    std::string getPath() {return _path;};
 
-		protected:
-			int _Write(const unsigned char *data, size_t length);
-			int _Read(unsigned char *data, size_t length);
+protected:
+    int _Write(const unsigned char *data, size_t length);
+    int _Read(unsigned char *data, size_t length);
 
-		protected:
-			hid_device* _handle;
-			unsigned short _vid;
-			unsigned short _pid;
-			std::string _path;
-			bool _bFirstCmd;
-		}; // class JubiterHidDevice end
+protected:
+    hid_device* _handle;
+    unsigned short _vid;
+    unsigned short _pid;
+    std::string _path;
+    bool _bFirstCmd;
+}; // class JubiterHidDevice end
 
-	}// namespace device end
+} // namespace device end
 } // namespace jub end
 
 #endif // HID_MODE
