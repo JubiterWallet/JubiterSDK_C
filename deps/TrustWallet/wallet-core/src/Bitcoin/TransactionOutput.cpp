8b82be254954329460a26fbda6f9347cb6b6df0d
panmin2256 <panmin2256@ftsafe.com>
2020-01-21 14:42:15 +0800

    Decoding for OutPoint, Script, TransactionInput, TransactionOutput and Transaction done; Deserializing for tx done; SegWit tx deserialzation not finish yet.

--- deps/TrustWallet/wallet-core/src/Bitcoin/TransactionOutput.cpp
+++ deps/TrustWallet/wallet-core/src/Bitcoin/TransactionOutput.cpp
@@ -14,3 +14,20 @@ void TransactionOutput::encode(std::vector<uint8_t>& data) const {
     encode64LE(value, data);
     script.encode(data);
 }
+
+// JuBiter-defined
+/// Decodes the provided buffer into the output.
+bool TransactionOutput::decode(const std::vector<uint8_t>& data) {
+
+    size_t index = 0;
+    value = decode64LE(&data[0]);
+    index += (sizeof(value)/sizeof(uint8_t));
+
+    std::vector<uint8_t> temp(std::begin(data)+index, std::end(data));
+    return script.decode(temp);
+}
+
+// JuBiter-defined
+size_t TransactionOutput::size() {
+    return ((sizeof(value)/sizeof(uint8_t)) + script.size());
+}
