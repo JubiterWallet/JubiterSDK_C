2a7e1be0cdfd76673ae45d19877ac2c8a2e599fb
ZXC-zxc <xiaochen@ftsafe.com>
2020-01-23 10:27:16 +0800

    BTC token add unsignedTX

--- include/token/BTC/BaseBTCImpl.h
+++ include/token/BTC/BaseBTCImpl.h
@@ -12,6 +12,13 @@ namespace jub {
 			};
 
 
+			virtual JUB_RV SerializeUnsignedTx(const JUB_ENUM_BTC_TRANS_TYPE& type,
+				const std::vector<INPUT_BTC>& vInputs,
+				const std::vector<OUTPUT_BTC>& vOutputs,
+				const JUB_UINT32 lockTime,
+				uchar_vector& unsignedRaw);
+
+
 		protected:
 			// add curve, prefix here
 			TWCoinType _coin;
