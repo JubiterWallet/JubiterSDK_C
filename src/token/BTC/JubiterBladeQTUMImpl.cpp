cead4d167e68b9663c3e13bf2d0e076b8d01942e
panmin2256 <panmin2256@ftsafe.com>
2020-02-11 17:11:12 +0800

    TWBitcoinSigHashTypeAllFork defined.

--- src/token/BTC/JubiterBladeQTUMImpl.cpp
+++ src/token/BTC/JubiterBladeQTUMImpl.cpp
@@ -4,29 +4,32 @@
 
 
 namespace jub {
-	namespace token {
-		JUB_RV JubiterBladeQTUMImpl::SetQRC20ETHToken(const std::string& tokenName,
-			const JUB_UINT16 unitDP,
-			const std::string& contractAddress) {
+namespace token {
 
-			uchar_vector lvName = Tollv(tokenName);
-			uchar_vector address;
-			address << ETHHexStr2CharPtr(contractAddress);
 
-			uchar_vector data;
-			data << (uint8_t)unitDP;
-			data << (uint8_t)lvName.size();
-			data << lvName;
-			data << (uint8_t)address.size();
-			data << address;
+JUB_RV JubiterBladeQTUMImpl::SetQRC20ETHToken(const std::string& tokenName,
+                                              const JUB_UINT16 unitDP,
+                                              const std::string& contractAddress) {
 
-			APDU apdu(0x00, 0xc7, 0x00, 0x00, (JUB_ULONG)data.size(), data.data());
-			JUB_UINT16 ret = 0;
-			JUB_VERIFY_RV(_SendApdu(&apdu, ret));
-			JUB_VERIFY_COS_ERROR(ret);
+    uchar_vector lvName = Tollv(tokenName);
+    uchar_vector address;
+    address << ETHHexStr2CharPtr(contractAddress);
 
-			return JUBR_OK;
-		}
-	}
+    uchar_vector data;
+    data << (uint8_t)unitDP;
+    data << (uint8_t)lvName.size();
+    data << lvName;
+    data << (uint8_t)address.size();
+    data << address;
+
+    APDU apdu(0x00, 0xc7, 0x00, 0x00, (JUB_ULONG)data.size(), data.data());
+    JUB_UINT16 ret = 0;
+    JUB_VERIFY_RV(_SendApdu(&apdu, ret));
+    JUB_VERIFY_COS_ERROR(ret);
+
+    return JUBR_OK;
 }
 
+
+} // namespace token end
+} // namespace jub end
