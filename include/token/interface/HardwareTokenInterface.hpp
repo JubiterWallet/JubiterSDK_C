ed1fdff680e2ded653d265e73c482f0486a13474
panmin2256 <panmin2256@ftsafe.com>
2020-09-23 20:22:10 +0800

    JUB_ExportMnemonic() for NFC done.

--- include/token/interface/HardwareTokenInterface.hpp
+++ include/token/interface/HardwareTokenInterface.hpp
@@ -49,12 +49,8 @@ public:
 
     virtual JUB_RV ImportMnemonic(const std::string& pinMix,
                                   const std::string& mnemonic) = 0;
-    virtual JUB_RV SetMnemonic(const std::string& pinMix,
-                               const JUB_ENUM_MNEMONIC_STRENGTH& strength,
-                               const std::string& entropy,
-                               const std::string& seed) = 0;
-    virtual JUB_RV GetMnemonic(const std::string& pinMix,
-                               OUT std::string& mnemonic) = 0;
+    virtual JUB_RV ExportMnemonic(const std::string& pinMix,
+                                  OUT std::string& mnemonic) = 0;
     virtual bool   HasRootKey() = 0;
 
     // BIO
