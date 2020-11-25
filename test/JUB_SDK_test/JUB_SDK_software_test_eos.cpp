64fa0cb7aee15d2ae9887fc314aeed342dc9d5c9
panmin2256 <panmin2256@ftsafe.com>
2020-11-25 18:07:14 +0800

    Support Tron, hw not finish yet.

---
+++ test/JUB_SDK_test/JUB_SDK_software_test_eos.cpp
@@ -0,0 +1,127 @@
+//
+//  JUB_SDK_software_test_eos.cpp
+//  JubSDKTest
+//
+//  Created by panmin on 2019/9/19.
+//  Copyright © 2019 JuBiter. All rights reserved.
+//
+
+#include "JUB_SDK_test.h"
+#include "JUB_SDK_test_eos.hpp"
+#include "JUB_SDK_software_test_eos.hpp"
+
+#include "mSIGNA/stdutils/uchar_vector.h"
+#include "JUB_SDK_main.h"
+#include "Base58.h"
+
+void software_test_eos(const char* json_file) {
+
+    Json::Value root = readJSON(json_file);
+
+    JUB_RV rv = JUBR_ERROR;
+
+/*    JUB_CHAR_PTR mnemonic = nullptr;
+    JUB_RV rv = JUB_GenerateMnemonic_soft(STRENGTH128, &mnemonic);
+    if(rv == JUBR_OK) {
+        cout << mnemonic << endl;
+    }
+
+    rv = JUB_CheckMnemonic(mnemonic);
+    if(rv != JUBR_OK) {
+        cout << "JUB_CheckMnemonic return" << rv << endl;
+    }
+*/
+    JUB_BYTE seed[64] = {0,};
+    JUB_UINT16 seedLen = sizeof(seed)/sizeof(JUB_BYTE);
+    auto callback = [](JUB_UINT32 current, JUB_UINT32 total) -> void {
+        cout << ".";
+    };
+
+    JUB_CHAR_CPTR mnemonic = "gauge hole clog property soccer idea cycle stadium utility slice hold chief";
+    rv = JUB_GenerateSeed_soft(mnemonic, "", seed, callback);
+    if (rv != JUBR_OK) {
+        cout << "JUB_GenerateSeed_soft error" << endl;
+    }
+    uchar_vector vSeed(seedLen);
+    for (int i=0; i<seedLen; ++i) {
+        vSeed[i] = seed[i];
+    }
+    cout << "seed: " << vSeed.getHex() << endl;
+    cout << endl;
+
+    JUB_CHAR_PTR masterXprv = nullptr;
+    rv = JUB_SeedToMasterPrivateKey_soft(seed, seedLen,
+                                         JUB_ENUM_CURVES::SECP256K1,
+//                                         JUB_ENUM_CURVES::ED25519,
+                                         &masterXprv);
+    if (rv == JUBR_OK) {
+        cout << masterXprv << endl;
+    }
+
+    CONTEXT_CONFIG_EOS cfg;
+    cfg.mainPath = (char*)root["main_path"].asCString();
+    JUB_UINT16 contextID;
+    rv = JUB_CreateContextEOS_soft(cfg, masterXprv, &contextID);
+    if (rv != JUBR_OK) {
+        cout << "JUB_CreateContextEOS_soft return " << rv << endl;
+        return;
+    }
+
+    JUB_CHAR_PTR mainPub = nullptr;
+    rv = JUB_GetMainHDNodeEOS(contextID, JUB_ENUM_PUB_FORMAT::HEX, &mainPub);
+    if (rv != JUBR_OK) {
+        cout << "JUB_GetMainHDNodeEOS return " << rv << endl;
+        return;
+    }
+    cout << "JUB_GetMainHDNodeEOS return " << mainPub << endl;
+    JUB_FreeMemory(mainPub);
+
+    rv = JUB_GetMainHDNodeEOS(contextID, JUB_ENUM_PUB_FORMAT::XPUB, &mainPub);
+    if (rv != JUBR_OK) {
+        cout << "JUB_GetMainHDNodeEOS return " << rv << endl;
+        return;
+    }
+    cout << "JUB_GetMainHDNodeEOS return " << mainPub << endl;
+    JUB_FreeMemory(mainPub);
+
+    BIP44_Path path;
+    path.change = (JUB_ENUM_BOOL)root["EOS"]["bip32_path"]["change"].asBool();
+    path.addressIndex = root["EOS"]["bip32_path"]["addressIndex"].asInt();
+//    BIP48_Path path;
+//    path.network = 4;
+//    path.role = JUB_ENUM_GRAPHENE_ROLE::ACTIVE;
+//    path.accountIndex = 0;
+//    path.keyIndex = 0;
+    JUB_CHAR_PTR pub = nullptr;
+    rv = JUB_GetHDNodeEOS(contextID, JUB_ENUM_PUB_FORMAT::HEX, path, &pub);
+    if (rv != JUBR_OK) {
+        cout << "JUB_GetHDNodeEOS return " << rv << endl;
+        return;
+    }
+    cout << "JUB_GetHDNodeEOS return " << pub << endl;
+    JUB_FreeMemory(pub);
+
+    rv = JUB_GetHDNodeEOS(contextID, JUB_ENUM_PUB_FORMAT::XPUB, path, &pub);
+    if (rv != JUBR_OK) {
+        cout << "JUB_GetHDNodeEOS return " << rv << endl;
+        return;
+    }
+    cout << "JUB_GetHDNodeEOS return " << pub << endl;
+    JUB_FreeMemory(pub);
+
+    JUB_CHAR_PTR address = nullptr;
+    rv = JUB_GetAddressEOS(contextID,
+                           path,
+                           JUB_ENUM_BOOL::BOOL_FALSE,
+                           &address);
+    if (rv != JUBR_OK) {
+        cout << "JUB_GetAddressEOS return " << rv << endl;
+    }
+    cout << "JUB_GetAddressEOS return " << address << endl;
+    JUB_FreeMemory(address);
+
+    rv = transaction_proc_EOS(contextID, root);
+    if (JUBR_OK != rv) {
+        return;
+    }
+}
---
+++ test/JUB_SDK_test/JUB_SDK_software_test_trx.cpp
@@ -0,0 +1,144 @@
+//
+//  JUB_SDK_software_test_trx.cpp
+//  JubSDKTest
+//
+//  Created by panmin on 2020/9/11.
+//  Copyright © 2020 JuBiter. All rights reserved.
+//
+
+#include "JUB_SDK_software_test_trx.hpp"
+#include "JUB_SDK_test_trx.hpp"
+
+#include "mSIGNA/stdutils/uchar_vector.h"
+#include "JUB_SDK_main.h"
+#include "Tron/Address.h"
+//#include "Tron/Signer.h"
+#include "PublicKey.h"
+#include "PrivateKey.h"
+
+//#include "xrp_pb_test.hpp"
+//#include "xrp_pb_test.c"
+
+void software_test_trx(const char* json_file) {
+//    simple_pb_test();
+//    trx_transfer_asset_pb_test();
+//    trx_pb_test();
+//    return;
+
+    JUB_RV rv = JUBR_ERROR;
+
+    //priv: 2d8f68944bdbfbc0769542fba8fc2d2a3de67393334471624364c7006da2aa54
+    TW::PrivateKey privateKey = TW::PrivateKey(uchar_vector("2d8f68944bdbfbc0769542fba8fc2d2a3de67393334471624364c7006da2aa54"));
+    const auto publicKey = privateKey.getPublicKey(TWPublicKeyTypeSECP256k1Extended);
+    const auto addr = TW::Tron::Address(publicKey);
+    std::cout << "addr: " << addr.string() << std::endl;
+
+
+    Json::Value root = readJSON(json_file);
+
+//    JUB_CHAR_PTR mnemonic = nullptr;
+//
+//    rv = JUB_GenerateMnemonic_soft(STRENGTH128, &mnemonic);
+//    if(rv == JUBR_OK) {
+//        cout << "JUB_GenerateMnemonic_soft() return " << mnemonic << endl;
+//    }
+
+    JUB_CHAR_CPTR mnemonic = "gauge hole clog property soccer idea cycle stadium utility slice hold chief";
+
+    rv = JUB_CheckMnemonic(mnemonic);
+    if(rv != JUBR_OK) {
+        cout << "JUB_CheckMnemonic() return" << rv << endl;
+    }
+    JUB_BYTE seed[64] = {0,};
+    JUB_UINT16 seedLen = sizeof(seed)/sizeof(JUB_BYTE);
+    auto callback = [](JUB_UINT32 current, JUB_UINT32 total) -> void {
+        cout << ".";
+    };
+
+    rv = JUB_GenerateSeed_soft(mnemonic, "", seed, callback);
+    if (rv != JUBR_OK) {
+        cout << "JUB_GenerateSeed_soft() error" << endl;
+    }
+    uchar_vector vSeed(seedLen);
+    for (int i=0; i<seedLen; ++i) {
+        vSeed[i] = seed[i];
+    }
+    cout << "seed: " << vSeed.getHex() << endl;
+    cout << endl;
+
+    JUB_CHAR_PTR masterXprv = nullptr;
+    rv = JUB_SeedToMasterPrivateKey_soft(seed, seedLen,
+                                         JUB_ENUM_CURVES::SECP256K1,
+                                         &masterXprv);
+    if (rv == JUBR_OK) {
+        cout << "JUB_SeedToMasterPrivateKey_soft() return " << masterXprv << endl;
+    }
+
+    CONTEXT_CONFIG_TRX cfgeth;
+    cfgeth.mainPath = (char*)root["main_path"].asCString();
+    JUB_UINT16 contextID = 0;
+    rv = JUB_CreateContextTRX_soft(cfgeth, masterXprv, &contextID);
+    if (rv != JUBR_OK) {
+        cout << "JUB_CreateContextETH_soft() return " << rv << endl;
+    }
+
+    JUB_CHAR_PTR xpub = nullptr;
+    rv = JUB_GetMainHDNodeTRX(contextID, JUB_ENUM_PUB_FORMAT::HEX, &xpub);
+    if (rv != JUBR_OK) {
+        cout << "JUB_GetMainHDNodeTRX() return " << rv << endl;
+    }
+    else {
+        cout << "JUB_GetMainHDNodeTRX() in HEX: " << xpub << endl;
+        JUB_FreeMemory(xpub);
+    }
+
+    rv = JUB_GetMainHDNodeTRX(contextID, JUB_ENUM_PUB_FORMAT::XPUB, &xpub);
+    if (rv != JUBR_OK) {
+        cout << "JUB_GetMainHDNodeTRX() return " << rv << endl;
+    }
+    else {
+        cout << "JUB_GetMainHDNodeTRX() in XPUB: " << xpub << endl;
+        JUB_FreeMemory(xpub);
+    }
+
+    BIP44_Path path;
+    path.change = (JUB_ENUM_BOOL)root["TRX"]["bip32_path"]["change"].asBool();
+    path.addressIndex = root["TRX"]["bip32_path"]["addressIndex"].asInt();
+    rv = JUB_GetHDNodeTRX(contextID, JUB_ENUM_PUB_FORMAT::HEX, path, &xpub);
+    if (rv != JUBR_OK) {
+        cout << "JUB_GetHDNodeTRX() return " << rv << endl;
+    }
+    else {
+        cout << "JUB_GetHDNodeTRX() in HEX: " << xpub << endl;
+        JUB_FreeMemory(xpub);
+    }
+
+    rv = JUB_GetHDNodeTRX(contextID, JUB_ENUM_PUB_FORMAT::XPUB, path, &xpub);
+    if (rv != JUBR_OK) {
+        cout << "JUB_GetHDNodeTRX() return " << rv << endl;
+    }
+    else {
+        cout << "JUB_GetHDNodeTRX() in XPUB: " << xpub << endl;
+        JUB_FreeMemory(xpub);
+    }
+
+    JUB_CHAR_PTR address = nullptr;
+    rv = JUB_GetAddressTRX(contextID, path, BOOL_FALSE, &address);
+    if(rv != JUBR_OK) {
+        cout << "JUB_GetAddressTRX() return " << rv << endl;
+    }
+    else {
+        cout << "JUB_GetAddressTRX() return " << address << endl;
+        JUB_FreeMemory(address);
+    }
+
+    rv = transaction_proc_TRX(contextID, root);
+    if (JUBR_OK != rv) {
+        return;
+    }
+
+//    rv = transaction_proc_TRC20(contextID, root);
+//    if (JUBR_OK != rv) {
+//        return;
+//    }
+}
