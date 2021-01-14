25fa39be07191e57493fcf4834777b903027af8f
panmin2256 <panmin2256@ftsafe.com>
2021-01-14 14:56:38 +0800

    Fix bugs of test.

--- test/JUB_SDK_test/JUB_SDK_test_btc.cpp
+++ test/JUB_SDK_test/JUB_SDK_test_btc.cpp
@@ -377,6 +377,13 @@ JUB_RV transactionUSDT_proc(JUB_UINT16 contextID, Json::Value root) {
             if (output.stdOutput.changeAddress) {
                 output.stdOutput.path.change = (JUB_ENUM_BOOL)root["outputs"][i]["bip32_path"]["change"].asBool();
                 output.stdOutput.path.addressIndex = root["outputs"][i]["bip32_path"]["addressIndex"].asInt();
+
+                JUB_CHAR_PTR selfAddress;
+                rv = JUB_GetAddressBTC(contextID, output.stdOutput.path, JUB_ENUM_BOOL::BOOL_FALSE, &selfAddress);
+                if (JUBR_OK != rv) {
+                    return rv;
+                }
+                output.stdOutput.address = selfAddress;
             }
             outputs.push_back(output);
         }
@@ -395,6 +402,14 @@ JUB_RV transactionUSDT_proc(JUB_UINT16 contextID, Json::Value root) {
                 std::cout << "amount: " << USDT_outputs[i].stdOutput.amount << std::endl;
                 std::cout << "change: " << USDT_outputs[i].stdOutput.changeAddress << std::endl;
                 std::cout << "addressIndex: " << USDT_outputs[i].stdOutput.path.addressIndex << std::endl;
+                if (USDT_outputs[i].stdOutput.changeAddress) {
+                    JUB_CHAR_PTR selfAddress;
+                    rv = JUB_GetAddressBTC(contextID, USDT_outputs[i].stdOutput.path, JUB_ENUM_BOOL::BOOL_FALSE, &selfAddress);
+                    if (JUBR_OK != rv) {
+                        return rv;
+                    }
+                    USDT_outputs[i].stdOutput.address = selfAddress;
+                }
                 break;
             case JUB_ENUM_SCRIPT_BTC_TYPE::RETURN0:
                 std::cout << "JUB_ENUM_SCRIPT_BTC_TYPE::RETURN0:" << std::endl;
