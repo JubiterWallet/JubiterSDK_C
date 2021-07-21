056f32d9eff856777f3d2a1347336d8c353e4def
weijia <378437002@qq.com>
2021-07-21 16:53:42 +0800

    Add sr25519 as a submodule.

--- deps/sr25519-crust/build.rs
+++
@@ -1,8 +0,0 @@
-use std::path::Path;
-
-extern crate cbindgen;
-use cbindgen::generate;
-
-fn main() {
-    generate(Path::new(".")).unwrap().write_to_file(Path::new("./include/sr25519/sr25519.h"));
-}
